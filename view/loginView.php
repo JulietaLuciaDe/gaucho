79974e81d24fc0a4f8a8d94ece9be26a10a5019d
cpinaud <cintiapinaud@gmail.com>
2022-05-22 01:02:41 -0300

    valida registro

--- view/loginView.php
+++ view/loginView.php
@@ -1,9 +1,3 @@
 <div class="contenido">
-    <form action="index.php?module=logIn&method=validarSesion" method="POST">
-        <label for="usuario">Usuario:</label>
-        <input type="text" name="usuario">
-        <label for="password">Clave:</label>
-        <input type="password" name="password">
-        <input type="submit">
-    </form>
+    <p>ACA VA LA VISTA DEL LOGIN</p>
 </div>
