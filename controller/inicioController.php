89773532e9de319e4fc0c267a9a51d3107a63102
cpinaud <cintiapinaud@gmail.com>
2022-06-21 22:46:39 -0300

    se corrige validador de fechas en validatorHelper para que funcione y se implementa en el buscador

--- controller/inicioController.php
+++ controller/inicioController.php
@@ -26,16 +26,17 @@ class InicioController {
           }
           //TODO: BOTON SUBMIT NO LO RECONOCE POR EL JS, POR EL MOMENTO VALIDAMOS CON ORIGEN
         if(isset($_POST['origen'])){   
-          //FALTA VALIDAR FECHA(AGREGAR EN EL VALIDATOR), TIPOVUELO ( Y NO SÉ SI ES NECESARIO LOS RADIO TAMBIEN)  
+          //FALTA VALIDAR TIPOVUELO ( Y NO SÉ SI ES NECESARIO LOS RADIO TAMBIEN)  
           if(   ValidatorHelper::validacionDeTexto($_POST["origen"],20)&&
-                ValidatorHelper::validacionDeTexto($_POST["destino"],20)){
+                ValidatorHelper::validacionDeTexto($_POST["destino"],20)&&
+                ValidatorHelper::validacionDeFecha($_POST["fechaIda"])){
                   $origen= $_POST["origen"];
                   $destino= $_POST["destino"];
                   $tipoVuelo = $_POST["tipoVuelo"];
                   $ida = $_POST["ida"];
                   $fechaIda = $_POST["fechaIda"];
                   if($ida==1){
-                    if(isset($_POST["fechaVuelta"]) && !empty($_POST["fechaVuelta"])){
+                    if(ValidatorHelper::validacionDeFecha($_POST["fechaVuelta"])){
                       $fechaVuelta = $_POST["fechaVuelta"];
                       $whereVuelta = "OR (V.origen = '$destino' and V.destino = '$origen' and T.id= '$tipoVuelo' and V.fecha<='$fechaVuelta' and V.fecha>'$fechaIda')";
                     }                    
@@ -72,7 +73,7 @@ class InicioController {
           }else{
             $data = ["menu"=> $menu,"resultado" => $resultado,"noData"=>true];
             
-//Si al buscar un vuelo no esta, se debe crear en la base de datos:
+//Si al reservar un vuelo no esta, se debe crear en la base de datos:
            //   $this->inicioModel->registrarVuelo($origen,$destino,$tipoVuelo/*,$ida, $idaVuelta*/,$fechaIda,$fechaVuelta);
           }
         $this->printer->generateView('inicioView.html',$data);
